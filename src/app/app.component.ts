7b316642640874ac3ab5e3e0409db26c9d075233
jaimehu20 <jaimehu20@gmail.com>
2024-10-16 16:31:52 +0200

    ADD/ Dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetStartedComponent } from './pages/get-started/get-started.component';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GetStartedComponent } from './pages/get-started/get-started.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'get-started', component: GetStartedComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', redirectTo: '/get-started', pathMatch: 'full' }
+];
