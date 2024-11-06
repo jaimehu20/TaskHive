bd6d343537a3d91f121b226028f2a886356f56a8
jaimehu20 <jaimehu20@gmail.com>
2024-11-06 18:20:17 +0100

    ADD/ Tasks are mapped in dashboard

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Task {
+    taskName: string,
+    taskDescription: string,
+    deadLine: string,
+    startTime: string,
+    endTime: string,
+    priority: string,
+    status: string,
+    tags: [],
+    guests: []
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user/user.service';
 import { NavBarComponent } from "../../shared/components/nav-bar/nav-bar.component";
+import { Task } from "../../interfaces/interfaces"
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -16,6 +18,11 @@ export class DashboardComponent implements OnInit {
 
   userID: string | null = null;
   userData: any = null;
+  tasks: any = null;
+  todaysTasks: Task[] = [];
+  nearbyTasks: Task[] = [];
+  groupedTasks: { [date: string]: any[] } = {};
+  groupedKeys: string[] = [];
 
   constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) { }
 
@@ -27,12 +34,16 @@ export class DashboardComponent implements OnInit {
       this.userService.getUserInfo(this.userID).subscribe({
         next: (data: any) => {
           this.userData = data.user;
+          this.tasks = this.userData.tasks
         },
         error: (error: any) => {
           console.error(`Error fetching data:`, error)
         },
         complete: () => {
           this.userService.setUserInfo(this.userData)
+          this.taskFilter()
+          this.groupNearbyTasksByDate();
+          this.groupedKeys = Object.keys(this.groupedTasks);
           return
         }
       })
@@ -42,4 +53,30 @@ export class DashboardComponent implements OnInit {
   taskCreatorRedirect() {
     this.router.navigate(['/task-form'])
   }
+
+  taskFilter() {
+    const today = new Date().toISOString().slice(0, 10);
+
+    this.todaysTasks = this.tasks.filter((task: Task) => {
+      const taskDate  = task.deadLine.slice(0, 10);
+      return taskDate === today;
+    });
+
+    this.nearbyTasks = this.tasks.filter((task: Task) => {
+      const taskDate = task.deadLine.slice(0, 10);
+      return taskDate > today;
+    })
+  }
+
+  groupNearbyTasksByDate(): void {
+    this.groupedTasks = this.nearbyTasks.reduce((acc, task) => {
+      const date = task.deadLine;
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(task);
+      return acc
+    }, {} as { [date: string]: any[] });
+  }
+
 }
